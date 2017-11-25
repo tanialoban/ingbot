8d56bfe0f2ad76508642b4fe01b9662bc1a79a94
tanialoban <tanusha1391@gmail.com>
2017-11-25 15:41:23 +0300

    bot vers 1.7.0 with commands

--- btelegr.py
+++ btelegr.py
@@ -40,12 +40,126 @@ class BotHandler:
         thanks = ('всегда пожалуйста', 'я старался', 'не проблема', 'обращайтесь')  
         out_msg = ''
         msg = ""    
-        portals = db['portals']
         modes = db['portals.mods']  
         players = db['players']
+        portals = db['portals']
         chat_id = last['message']['chat']['id']
-        firstname = last['message']['chat']['first_name']
-        username = last['message']['chat']['username']           
+        
+        try:
+            firstname = last['message']['chat']['first_name']
+        except KeyError:
+            firstname = ''
+            username = last['message']['from']['first_name'] 
+        if '@bjornKingBot' in in_msg:              
+            in_msg = in_msg[:-13]
+        if in_msg == '/enl8':            
+            portE8 = portals.find({"level": "E8"})
+            msg = 'E8:'
+            i = 1
+            for port in portE8:
+                msg = msg + '\n' + str(i) + ". "+ port['name'] + " " + port['level'] + ' '
+                i =  i + 1
+            if i > 1:    
+                self.send_mess(chat_id, msg)     
+            else:
+                self.send_mess(chat_id, "нет E8")    
+        if in_msg == '/enl78': 
+            portE8 = portals.find({"level": "E8"})
+            portE7 = portals.find({"level": "E7"})
+            msg = 'E8:'
+            i = 1
+            for port in portE8:
+                msg = msg + '\n' + str(i) + ". "+ port['name'] + " " + port['level'] + ' '
+                i = i + 1
+            if i == 1:    
+                msg = ''
+            msg = msg + '\nE7:'
+            for port in portE7:
+                msg = msg + '\n' + str(i) + ". "+ port['name'] + " " + port['level'] + ' '
+                i = i + 1
+            if i > 1:    
+                self.send_mess(chat_id, msg) 
+            else:
+                self.send_mess(chat_id, "нет E7-E8")    
+        if in_msg == '/res78':  
+            portR8 = portals.find({"level": "R8"})
+            portR7 = portals.find({"level": "R7"})
+            msg = 'R8:'
+            i = 1
+            for port in portR8:
+                msg = msg + '\n' + str(i) + ". "+ port['name'] + " " + port['level'] + ' '
+                i = i + 1
+            if i == 1:    
+                msg = ''
+            msg = msg + '\nR7:'
+            for port in portR7:
+                msg = msg + '\n' + str(i) + ". "+ port['name']  + " " + port['level'] + ' '  
+                i = i + 1
+            if i > 1:    
+                self.send_mess(chat_id, msg) 
+            else:
+                self.send_mess(chat_id, "нет R7-R8")    
+        if in_msg == '/res8':   
+            portR8 = portals.find({"level": "R8"})
+            i = 1
+            msg = 'R8:'
+            for port in portR8:
+                msg = msg + '\n' + str(i) + ". "+ port['name'] + " " + port['level'] + ' '   
+                i = i + 1
+            if i > 1:    
+                self.send_mess(chat_id, msg) 
+            else:
+                self.send_mess(chat_id, "нет R8")        
+        if in_msg == '/eferm':
+            portE7 = portals.find({"level": "E7"})
+            portE8 = portals.find({"level": "E8"})
+            i = 1
+            msg = 'E8:\n'                   
+            for port in portE8:
+                moddb = modes.find({"_id": port['_id']})
+                for mods in moddb:   
+                    if 'Heat Sink' in mods['mod1'] or 'Multi-hack' in mods['mod1']  or 'Transmuter' in mods['mod1'] or 'Heat Sink' in mods['mod2'] or 'Multi-hack' in mods['mod2']  or 'Transmuter' in mods['mod2'] or 'Heat Sink' in mods['mod3'] or 'Multi-hack' in mods['mod3']  or 'Transmuter' in mods['mod3'] or 'Heat Sink' in mods['mod4'] or 'Multi-hack' in mods['mod4']  or 'Transmuter' in mods['mod4'] :    
+                        msg = msg + '\n' + str(i) + ". "+ port['name'] + " " + port['level'] + ' '
+                        i = i + 1
+
+            if i == 1:    
+                msg = ''
+            msg = msg + '\nE7:'
+            for port in portE7:
+                moddb = modes.find({"_id": port['_id']})
+                for mods in moddb:   
+                    if 'Heat Sink' in mods['mod1'] or 'Multi-hack' in mods['mod1']  or 'Transmuter' in mods['mod1'] or 'Heat Sink' in mods['mod2'] or 'Multi-hack' in mods['mod2']  or 'Transmuter' in mods['mod2'] or 'Heat Sink' in mods['mod3'] or 'Multi-hack' in mods['mod3']  or 'Transmuter' in mods['mod3'] or 'Heat Sink' in mods['mod4'] or 'Multi-hack' in mods['mod4']  or 'Transmuter' in mods['mod4'] :   
+                        msg = msg + '\n' + str(i) + ". "+ port['name']  + " " + port['level'] + ' '  
+                        i = i + 1
+            if i > 1:    
+                self.send_mess(chat_id, msg) 
+            else:
+                self.send_mess(chat_id, "нет ферм")    
+        if in_msg == '/rferm':
+            portR7 = portals.find({"level": "R7"})
+            portR8 = portals.find({"level": "R8"})
+            msg = 'R8:'
+            i = 1
+            for port in portR8:
+                moddb = modes.find({"_id": port['_id']})
+                for mods in moddb:   
+                    if 'Heat Sink' in mods['mod1'] or 'Multi-hack' in mods['mod1']  or 'Transmuter' in mods['mod1'] or 'Heat Sink' in mods['mod2'] or 'Multi-hack' in mods['mod2']  or 'Transmuter' in mods['mod2'] or 'Heat Sink' in mods['mod3'] or 'Multi-hack' in mods['mod3']  or 'Transmuter' in mods['mod3'] or 'Heat Sink' in mods['mod4'] or 'Multi-hack' in mods['mod4']  or 'Transmuter' in mods['mod4'] :   
+                        msg = msg + '\n' + str(i) + ". "+ port['name'] + " " + port['level'] + ' '
+                        i = i + 1
+            if i == 1:    
+                msg = ''
+            msg = msg + '\nR7:'
+            for port in portR7:
+                moddb = modes.find({"_id": port['_id']})
+                for mods in moddb: 
+                    if 'Heat Sink' in mods['mod1'] or 'Multi-hack' in mods['mod1']  or 'Transmuter' in mods['mod1'] or 'Heat Sink' in mods['mod2'] or 'Multi-hack' in mods['mod2']  or 'Transmuter' in mods['mod2'] or 'Heat Sink' in mods['mod3'] or 'Multi-hack' in mods['mod3']  or 'Transmuter' in mods['mod3'] or 'Heat Sink' in mods['mod4'] or 'Multi-hack' in mods['mod4']  or 'Transmuter' in mods['mod4'] :   
+                        msg = msg + '\n' + str(i) + ". "+ port['name'] + " " + port['level'] + ' '  
+                        i = i + 1
+            if i > 1:    
+                self.send_mess(chat_id, msg) 
+            else:
+                self.send_mess(chat_id, "нет ферм")     
+
         if 'бот' in in_msg or 'Бот' in in_msg:
             if "статус" in in_msg:
                 cursor = portals.find({"name": in_msg[11:]}) 
@@ -54,7 +168,7 @@ class BotHandler:
                     f = False
                     mods = modes.find({"_id": port['_id']}) 
                     for mod in mods:   
-                        msg = port['name'] + ":\n" +  mod["mod1"]+" | " + mod["mod2"]+" | " + mod["mod3"]+" | " + mod["mod4"]
+                        msg = port['name'] + ":\n" +  mod["mod1"]+" = " + mod["own1"]  + "\n"+ mod["mod2"]+" = "+ mod["own2"]  + "\n" + mod["mod3"]+" = " + mod["own3"]  + "\n" + mod["mod4"] + " = "+ mod["own4"]
                 if f:
                     msg =  "Упс, " + in_msg[11:] + " нет в моей базе"
                 self.send_mess(chat_id, msg)
@@ -67,34 +181,29 @@ class BotHandler:
                 if f:    
                     msg = "Прости друг, " + in_msg[8:] + " еще не добавил в базу, но я работаю над этим :)"
                 self.send_mess(chat_id, msg)
-        elif in_msg in welcome:
+        elif in_msg.lower() in welcome:
             self.send_mess(chat_id, random.SystemRandom().choice(welcome) + ', ' + firstname)
-        elif in_msg in cool:            
+        elif in_msg.lower() in cool:            
             self.send_mess(chat_id, random.SystemRandom().choice(thanks)) 
-        # elif in_msg == '/enl8':
-
-        # elif in_msg == '/enl78': 
-
-        # elif in_msg == '/res78':  
-
-        # elif in_msg == '/res8':   
-
-        # elif in_msg == '/eferm':
-
-        # elif in_msg == '/rferm':
-
         print(msg)
 
-    def get_status(self, db):
+    def get_status(self, db,chat_id):
         portals = db['portals']
         modes = db['portals.mods']  
-        cursor = portals.find({"mod": 'X'})        
+        cursor = portals.find()        
         for port in cursor:
             mods = modes.find({"_id": port['_id']}) 
+            msg = ''
             for mod in mods:   
-                msg = port['name'] + ":\n" +  mod["mod1"]+" | " + mod["mod2"]+" | " + mod["mod3"]+" | " + mod["mod4"]
-            self.send_mess(chat_id, msg)
-            db.portals.update({ 'name': port['name'] }, { 'name': port['name'], 'mod': "" } )
+                msg = port['name'] + ":\n" +  mod["mod1"]+" = " + mod["own1"]  + "\n"+ mod["mod2"]+" = "+ mod["own2"]  + "\n" + mod["mod3"]+" = " + mod["own3"]  + "\n" + mod["mod4"] + " = "+ mod["own4"]
+            # self.send_mess(chat_id, msg)
+            try: 
+                db.portals.update({"_id": port['_id']}, 
+                {"_id": port['_id'], "level": port['level'], "owner":  port['owner'], "name": port['name'], "lat": port['lat'], 'lng':port['lng'], 'mod': port['mod'] })
+            except KeyError:
+                db.portals.update({"_id": port['_id']}, 
+                {"_id": port['_id'], "level": port['level'], "owner":  '', "name": port['name'], "lat": port['lat'], 'lng':port['lng'], 'mod': port['mod'] })
+               
 
 def main():     
     greet_bot = BotHandler(TOKEN) 
@@ -108,8 +217,8 @@ def main():
         if update_id == last_update['update_id']:
             message = last_update['message']['text']
             greet_bot.switch(message, last_update, db)
-            update_id += 1        
-        greet_bot.get_status(db)  
+            greet_bot.get_status(db, last_update['message']['chat']['id'])  
+            update_id += 1                
         time.sleep(1)     
      
 if __name__ == '__main__':  
